6df0780b34e8ad9983604b91f3d957e882dda518
closedevice <closedevice@gmail.com>
2018-01-12 19:24:42 +0800

    add density util class.

---
+++ akit-common/src/main/java/com/closedevice/common/DensityUtil.java
@@ -0,0 +1,21 @@
+package com.closedevice.common;
+
+import android.content.Context;
+
+/**
+ * @author liudongdong
+ * @date 2018/1/12
+ */
+
+public class DensityUtil {
+
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+}
